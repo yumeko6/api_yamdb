e35891555a07616a32ae4cb4df6310b7bdd0915c
Виктор Тузовский <aletissarichard@gmail.com>
2022-05-08 23:05:39 +0300

    Added SimpleJWT

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
     'api',
     'reviews',
