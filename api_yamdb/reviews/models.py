0294b1411a1521dcf0ae7f1b3016f13d5797c206
Денис Баринов <denisbarinov96@icloud.com>
2022-05-08 21:55:23 +0300

    test1d

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Категории произведений."""
+    name = models.CharField(
+        'Название категории',
+        max_length=256
+    )
+    slug = models.SlugField(
+        'Уникальный адрес адрес',
+        max_length=30,
+        unique=True
+        )
+
+    class Meta:
+        verbose_name = 'Категория'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """Жанры произведений."""
+    name = models.CharField(
+        'Название жанра',
+        max_length=256)
+    slug = models.SlugField(
+        'Уникальный адрес жанра',
+        max_length=30,
+        unique=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Жанры'
+
+    def __str__(self) -> str:
+        return self.name
