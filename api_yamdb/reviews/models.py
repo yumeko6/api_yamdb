4771198910f8345c1c99bf9c876a9bce0a417e4c
Виктор Тузовский <aletissarichard@gmail.com>
2022-05-09 15:01:56 +0300

    User model relocated to users app

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
 
 
 class Category(models.Model):
@@ -40,21 +40,4 @@ class Genre(models.Model):
         return self.name
 
 
-class User(AbstractUser):
-    username = models.CharField(
-        'Username',
-        max_length=255,
-        unique=True
-    )
-    email = models.EmailField(
-        'Email Address',
-        unique=True
-    )
-    bio = models.TextField(
-        'Биография',
-        blank=True,
-    )
-    role = models.TextField(
-        'Роль',
-        blank=True,
-    )
+
