217216b2a6fd99c4562d551f73f76af45ca205ee
Виктор Тузовский <aletissarichard@gmail.com>
2022-05-12 01:14:36 +0300

    Minor changes

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -2,13 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 
-from users.views import SignUpViewSet, UserCreateViewSet, AuthViewSet
+from users.views import SignUpViewSet, AuthViewSet
+from users.views import UserCreateViewSet
 
 router = SimpleRouter()
 
 router.register('v1/auth/signup', SignUpViewSet)
 router.register('v1/auth/token', AuthViewSet)
 router.register('v1/users', UserCreateViewSet)
+# router.register('v1/users/me', CurrentUserViewSet) -> не работает :(
 
 urlpatterns = [
     path('', include(router.urls)),
