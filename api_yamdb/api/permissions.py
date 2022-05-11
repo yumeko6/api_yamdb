e5c908b618fde93b89219b0f7cac3e50cd95ec56
Виктор Тузовский <aletissarichard@gmail.com>
2022-05-12 01:13:28 +0300

    Minor changes

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -22,8 +22,8 @@ class AdminOrSuperuser(permissions.BasePermission):
 class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_admin
-                or request.user.is_moderator
+                or request.user.role == 'admin'
+                or request.user.role == 'moderator'
                 or obj.author == request.user)
 
     def has_permission(self, request, view):
