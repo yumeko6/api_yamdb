22e133e61eb775bf44713e87c86b6723144d4a57
Виктор Тузовский <vtuzovskiy@fura.ru>
2022-05-13 18:39:14 +0300

    Created token generation and auth

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,8 +1,11 @@
 import random
 
 from django.core.mail import EmailMessage
+from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets, mixins, filters, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -44,24 +47,46 @@ class AuthViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserAuthSerializer
 
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            'access': str(refresh.access_token),
+        }
+
+    def perform_create(self, serializer):
+        user = get_object_or_404(
+            User, username=serializer.validated_data['username']
+        )
+        if user:
+            return Response(self.get_tokens_for_user(user))
+
 
 class UserCreateViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
-    permission_classes = (AdminOrSuperuser,)
+    permission_classes = (IsAuthenticated, AdminOrSuperuser)
+    authentication_classes = [JWTAuthentication, ]
     pagination_class = PageNumberPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username',)
     lookup_field = 'username'
 
+    #def partial_update(self, request, *args, **kwargs):
+    #    kwargs['partial'] = True
+    #    user = self.request.user
+    #    if user.role == 'admin':
+    #        serializer = self.get_serializer(data=request.data)
+    #        if not serializer.is_valid():
+    #            return Response(status=status.HTTP_400_BAD_REQUEST)
+    #    return self.update(request, *args, **kwargs)
 
-class CurrentUserViewSet(viewsets.ModelViewSet):
+
+class CurrentUserViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+                         viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
-    # def retrieve(self, request, *args, **kwargs):
-    #    user = self.get_object()
-    #    serializer = self.get_serializer(user)
-    #    return Response(serializer.data)
-    # !!! Код временно не работает, необходимо реализовать url api/v1/users/me
+    def retrieve(self, request, *args, **kwargs):
+        pass
