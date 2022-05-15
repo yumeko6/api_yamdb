4105ac6d1c68af723dd514323fe8862ab5120204
Виктор Тузовский <aletissarichard@gmail.com>
2022-05-16 01:36:39 +0300

    Added paths to signup, token

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
-
-from users.views import SignUpViewSet, AuthViewSet
+from users.views import get_token, signup
 from users.views import UserCreateViewSet
 
-router = SimpleRouter()
+router = DefaultRouter()
 
-router.register('v1/auth/signup', SignUpViewSet)
-router.register('v1/auth/token', AuthViewSet)
 router.register('v1/users', UserCreateViewSet)
-# router.register('v1/users/me', CurrentUserViewSet) -> не работает :(
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('v1/auth/token/', get_token),
+    path('v1/auth/signup/', signup),
 ]
